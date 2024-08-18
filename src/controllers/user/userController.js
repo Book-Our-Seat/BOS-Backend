f6d23c77b6aab0f78722192ef76580ebe4621a65
Manjunath Vasam <mvasam@adobe.com>
2024-08-18 16:23:29 +0530

    Endpoint for user profile updates (user endpoint)

--- src/controllers/user/userController.js
+++ src/controllers/user/userController.js
@@ -5,7 +5,16 @@ const getUserInfoHandler = async (req, res, next) => {
     try {
         const userInfo = await UserModel.findOne({
             where: { role: "user", id: user.id },
-            attributes: ["id", "name", "email", "approvalStatus", "coins"],
+            attributes: [
+                "id",
+                "name",
+                "email",
+                "approvalStatus",
+                "coins",
+                "phone",
+                "email",
+                "address",
+            ],
         });
         res.status(200).json({ userInfo });
     } catch (error) {
@@ -14,6 +23,31 @@ const getUserInfoHandler = async (req, res, next) => {
     }
 };
 
+const updateUserInfoHandler = async (req, res, next) => {
+    const { id } = req.user;
+    try {
+        let { userInfo } = req.body;
+        const user = await UserModel.findOne({
+            where: { id },
+        });
+        if (!user) {
+            return res.status(404).json({ message: "No user found!" });
+        }
+        user.name = userInfo.name;
+        user.email = userInfo.email;
+        user.address = userInfo.address;
+        user.city = userInfo.city;
+        user.state = userInfo.state;
+        user.pincode = userInfo.pincode;
+        await user.save();
+        res.status(201).json({ message: "Success" });
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+};
+
 module.exports = {
     getUserInfoHandler,
+    updateUserInfoHandler,
 };
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,14 +1,10 @@
 const express = require("express");
 const { authorizeUserMiddleware } = require("../middlewares/authMiddleware");
-const {
-    getUsersHandler,
-    approveUserHandler,
-    updateCoinsHandler,
-} = require("../controllers/admin/adminController");
-const { getUserInfoHandler } = require("../controllers/user/userController");
+const { getUserInfoHandler, updateUserInfoHandler } = require("../controllers/user/userController");
 
 const userRouter = express.Router();
 
 userRouter.get("/info", authorizeUserMiddleware, getUserInfoHandler)
+userRouter.post("/info", authorizeUserMiddleware, updateUserInfoHandler)
 
 module.exports = userRouter;
