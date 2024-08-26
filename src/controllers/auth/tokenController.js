b3415150d65fd27b3b7842c23b97328584f3623e
Manjunath Vasam <mvasam@adobe.com>
2024-08-26 19:07:40 +0530

    Refresh Token API

--- src/controllers/auth/tokenController.js
+++ src/controllers/auth/tokenController.js
@@ -1,30 +1,32 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+
 const UserModel = require("../../models/UserModel");
 const { generateAccessToken } = require("../../services/tokenService");
 const TokenModel = require("../../models/Auth/TokenModel");
 
 const accessTokenHandler = async (req, res, next) => {
-    const { token } = req.body;
+    const { refreshToken } = req.body;
 
-    if (!token) {
-        return res.status(401).json({ message: "No token found!" });
+    if (!refreshToken) {
+        return res.status(403).json({ message: "No token found!" });
     }
 
-    const savedToken = await TokenModel.findOne({ where: { token } });
+    const savedToken = await TokenModel.findOne({
+        where: { token: refreshToken },
+    });
 
     if (!savedToken) {
-        return res.status(403).json({ message: "Invalid token!" });
+        return res.status(401).json({ message: "Invalid token!" });
     }
 
     try {
-        jwt.verify(token, process.env.REFRESH_SECRET_KEY);
+        jwt.verify(refreshToken, process.env.REFRESH_SECRET_KEY);
         const accessToken = generateAccessToken({ id: savedToken.userId });
         res.status(201).json({ accessToken });
     } catch (error) {
         next(error);
     }
 };
-
-
 
 module.exports = accessTokenHandler;
